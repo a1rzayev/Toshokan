08838594e689579538b448742f0bb0589eaa8e57
a1rzayev <asifrzayev2006@example.com>
2024-03-23 16:14:55 +0400

    create test http proj

--- ToshokanApp/Program.cs
+++ ToshokanApp/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+
+HttpListener httpListener = new HttpListener();
+
+const int port = 8080;
+httpListener.Prefixes.Add($"http://*:{port}/");
+
+Console.WriteLine($"server started on port {port}");
+
+httpListener.Start();
+
+while(true){
+    var context = await httpListener.GetContextAsync();
+
+    string? endpoint = context.Request.RawUrl;
+
+    Console.WriteLine($"endpoint: {endpoint}");
+
+    context.Response.Close();
+}
