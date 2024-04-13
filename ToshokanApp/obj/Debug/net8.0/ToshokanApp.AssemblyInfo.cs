c35c57d06a667fd56aa876cb899062968221ef8f
a1rzayev <asifrzayev2006@example.com>
2024-04-13 19:14:12 +0400

    initial commit

--- ToshokanApp/obj/Debug/net8.0/ToshokanApp.AssemblyInfo.cs
+++ ToshokanApp/obj/Debug/net8.0/ToshokanApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToshokanApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+523a50e4c53958ed45950f3f15fd9fe2bf32e466")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a6e26865075ec0e4c93b7d1ebb8c204eed7f5f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToshokanApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToshokanApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
