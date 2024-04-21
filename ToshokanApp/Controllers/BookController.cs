6b48486b9df27c0585be1ef302e4e2b20d0a6a45
a1rzayev <asifrzayev2006@example.com>
2024-04-21 21:42:18 +0400

    Add routing(GetByName & Add)

--- ToshokanApp/Controllers/BookController.cs
+++ ToshokanApp/Controllers/BookController.cs
@@ -7,6 +7,7 @@ namespace LibraryApp.Controllers
     public class BookController : Controller
     {
         private readonly string jsonPath = "Resources/books.json";
+        [HttpGet]
         public IActionResult Index(){
             return View();
         }
@@ -25,19 +26,21 @@ namespace LibraryApp.Controllers
         
         [HttpGet]
         [ActionName("GetByName")]
-        [Route("[controller]/[action]/{name}")]
-        public async Task<IActionResult> GetByName(string name)
+        public async Task<IActionResult> GetByName(string? name)
         {
             var booksJson = await System.IO.File.ReadAllTextAsync(jsonPath);
             var books = JsonSerializer.Deserialize<IEnumerable<Book>>(booksJson, new JsonSerializerOptions {
                 PropertyNameCaseInsensitive = true
             });
-            IEnumerable<Book> booksByName = new List<Book>();
-            name.ToLower().Trim();
+            List<Book> booksByName = new List<Book>();
             foreach (var book in books){
-                if (book.Name.ToLower().Trim() == name) booksByName.Append(book);
+                if (book.Name?.ToLower().Trim() == name?.ToLower().Trim()){
+                    booksByName.Add(book);
+                }
             }
-            return Ok(booksByName);
+            ViewData["search"] = name;
+            return View(model: booksByName);
+
         }
         public IActionResult Privacy()
         {
@@ -47,7 +50,7 @@ namespace LibraryApp.Controllers
         [HttpPost]
         [ActionName("Add")]
         [Route("[controller]")]
-        public async Task<IActionResult> Add(Book newBook) {
+        public async Task<IActionResult> Add([FromForm] Book newBook) {
             var booksJson = await System.IO.File.ReadAllTextAsync(jsonPath);
 
             var books = JsonSerializer.Deserialize<List<Book>>(booksJson, new JsonSerializerOptions {
--- ToshokanApp/obj/Debug/net8.0/ToshokanApp.AssemblyInfo.cs
+++ ToshokanApp/obj/Debug/net8.0/ToshokanApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToshokanApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7bbc7f44c9346f726975a9cd7d00d436003274f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e708960396cf2c1505be4ea338aae72f91aff1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToshokanApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToshokanApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
