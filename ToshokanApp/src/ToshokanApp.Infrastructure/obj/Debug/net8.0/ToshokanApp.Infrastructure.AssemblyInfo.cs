5f3a28aca661dd8504f0e5726ab17cef965a170a
a1rzayev <asifrzayev2006@example.com>
2024-07-08 13:17:19 +0400

    Fix layout.cshtml

--- ToshokanApp/src/ToshokanApp.Infrastructure/obj/Debug/net8.0/ToshokanApp.Infrastructure.AssemblyInfo.cs
+++ ToshokanApp/src/ToshokanApp.Infrastructure/obj/Debug/net8.0/ToshokanApp.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToshokanApp.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81332088ff112cdd76354b025161cce009453a06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ae85d90e4aed8e9e20c813dc337c58858538ea2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToshokanApp.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToshokanApp.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
