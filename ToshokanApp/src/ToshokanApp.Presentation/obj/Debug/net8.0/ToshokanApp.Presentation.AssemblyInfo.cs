cbd5174ad13b11049c5ea7594a3a8c9d3e0dbdbd
a1rzayev <asifrzayev2006@example.com>
2024-11-23 23:35:13 +0400

    fix validations

--- ToshokanApp/src/ToshokanApp.Presentation/obj/Debug/net8.0/ToshokanApp.Presentation.AssemblyInfo.cs
+++ ToshokanApp/src/ToshokanApp.Presentation/obj/Debug/net8.0/ToshokanApp.Presentation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToshokanApp.Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f3a28aca661dd8504f0e5726ab17cef965a170a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+736151c63d9f8d3f23c8804fc49815b93802eca6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToshokanApp.Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToshokanApp.Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
